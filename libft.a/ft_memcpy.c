1562b1c2935a035844f27c92aab40380148d6b16
shinowtf <kahchun774@gmail.com>
2023-10-19 17:07:04 +0800

    ss

---
+++ libft.a/ft_memcpy.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lkah-chu <lkah-chu@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/17 21:09:36 by lkah-chu          #+#    #+#             */
+/*   Updated: 2023/10/17 22:10:37 by lkah-chu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	char *d;
+	const char *s;
+
+	d = dest;
+	s = src;
+	if (dest == NULL && src == NULL)
+		return(0);
+	if (n == 0)
+		return(dest);
+	while (n-- > 0)
+	{
+		*d = *s;
+		d++;
+		s++;
+	}
+	return (dest);
+}
---
+++ libft.a/libft.h
@@ -0,0 +1,18 @@
+#ifndef LIBFT_H
+#define LIBFT_H
+
+#include <stdlib.h>
+#include <unistd.h>
+
+int ft_isalpha(int c);
+int ft_isascii(int c);
+int ft_isdigit(int c);
+int ft_isalnum(int c);
+int ft_isprint(int c);
+
+size_t ft_strlen(const char *s);
+
+
+
+
+#endif
