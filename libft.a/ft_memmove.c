b3733db46a2a3b85d3e1161a6231e8ebf20c3c5b
shinowtf <kahchun774@gmail.com>
2023-10-21 11:04:35 +0800

    passup ytd work

---
+++ libft.a/ft_memmove.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lkah-chu <lkah-chu@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/20 11:24:42 by lkah-chu          #+#    #+#             */
+/*   Updated: 2023/10/20 15:22:24 by lkah-chu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dst, const void *src, size_t n)
+{
+	char *pdst = dst;
+	const char *psrc = src;
+
+	if (src == NULL && dst == NULL)
+		return (dst);
+	if (dst > src)
+	{
+		pdst += n;
+		psrc += n;
+		while (n-- > 0)
+		{
+			*(--pdst) = *(--psrc);
+		}
+	}
+	else
+	{
+		while (n-- > 0)
+        	{
+			*(pdst++) = *(psrc++);
+        	}
+	}
+	return (dst);
+}
