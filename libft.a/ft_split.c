627ce9314fa17511788f75a5b88d792f9b556efc
shinowtf <kahchun774@gmail.com>
2023-10-29 16:45:19 +0800

    complete split

--- libft.a/ft_split.c
+++ libft.a/ft_split.c
@@ -5,14 +5,69 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: lkah-chu <lkah-chu@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/28 16:32:03 by lkah-chu          #+#    #+#             */
-/*   Updated: 2023/10/28 16:32:38 by lkah-chu         ###   ########.fr       */
+/*   Created: 2023/10/29 15:01:42 by lkah-chu          #+#    #+#             */
+/*   Updated: 2023/10/29 16:27:39 by lkah-chu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static	int	ft_countwrd(char const *s, char c)
+{
+	unsigned int i;
+	int	counter;
+
+	i = 0;
+	counter = 0;
+	while(s[i])
+	{
+		while(s[i] == c)
+			i++;
+		if(s[i] != '\0')
+			counter++;
+		while(s[i] && (s[i] != c))
+			i++;
+	}
+	return (counter);
+}
+
+static	char	*ft_strndup(const char *s, size_t n)
+{
+	char	*str;
+
+	str = (char *)malloc(sizeof(char) * n + 1);
+	if (str == NULL)
+		return(NULL);
+	str = ft_strncpy(str, s, n);
+	str[n] = '\0';
+	return(str);
+}
+
 char	**ft_split(char const *s, char c)
 {
+	int	i;
+	int	j;
+	int	k;
+	char	**tab;
 
+	i = 0;
+	k = 0;
+	tab = (char **)malloc(sizeof(char *) * (ft_countword(s, c) + 1));
+	if (tab == NULL)
+		return(NULL);
+	while (s[i])
+	{
+		while(s1[i] == c)
+			i++;
+		j = i;
+		while (s[i]  && s[i] != c)
+			i++;
+		if (i > j)
+		{
+			tab[k] = ft_strndup(s + j, i - j);
+			k++;
+		}
+	}
+	tab[k] = NULL;
+	return (tab);
 }
