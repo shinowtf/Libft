4ffe7f187135fe2e3bccdb4b56984fa0a763fbe6
shinowtf <kahchun774@gmail.com>
2023-12-09 16:49:21 +0800

    debug time

--- libft.a/ft_striteri.c
+++ libft.a/ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: lkah-chu <lkah-chu@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/08 16:54:48 by lkah-chu          #+#    #+#             */
-/*   Updated: 2023/12/08 16:57:38 by lkah-chu         ###   ########.fr       */
+/*   Updated: 2023/12/09 16:29:06 by lkah-chu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,12 @@
 
 void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
-	
+	unsigned int	i;
+
+	i = 0;
+	while(s[i] != '0')
+	{
+		f(i, s + i);
+		i++;
+	}
 }
